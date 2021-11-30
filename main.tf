fcfe0b4a9f1ea02ca35f5c9843754db4c98f2c0c
sheezanrasool <sheezanrasool37@Agmail.com>
2021-11-30 14:47:00 +0530

    configure web server user data

--- main.tf
+++ main.tf
@@ -5,6 +5,11 @@ provider "aws" {
 resource "aws_instance" "Server" {
   ami = "ami-0279c3b3186e54acd"
   instance_type = "t2.micro"
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
   tags = {
     "Name" = "Web Server"
   }
